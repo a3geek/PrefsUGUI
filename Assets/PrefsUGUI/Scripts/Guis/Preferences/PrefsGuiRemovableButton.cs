dcc94952fea892d0c7ec5687e96d19f28693b473
a3geek <a3geek@gmail.com>
2020-06-19 03:27:39 +0900

    RemovableButtonを実装中

---
+++ Assets/PrefsUGUI/Scripts/Guis/Preferences/PrefsGuiRemovableButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace PrefsUGUI.Guis.Preferences
+{
+    [Serializable]
+    public class PrefsGuiRemovableButton : PrefsGuiButton
+    {
+        [SerializeField]
+        protected Button removeButton = null;
+
+        protected UnityAction onRemoveClicked = null;
+
+
+        protected override void Reset()
+        {
+            base.Reset();
+            this.removeButton = this.GetComponentsInChildren<Button>()?.LastOrDefault();
+        }
+    }
+}
