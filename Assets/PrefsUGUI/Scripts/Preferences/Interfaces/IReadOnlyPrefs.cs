7aec0e890e5d6b42f5dc9c4becd0b081abe0c32e
a3geek <a3geek@gmail.com>
2020-07-28 18:35:37 +0900

    IReadOnlyPrefsはIPrefsCommonを継承する

--- Assets/PrefsUGUI/Scripts/Preferences/Interfaces/IReadOnlyPrefs.cs
+++ Assets/PrefsUGUI/Scripts/Preferences/Interfaces/IReadOnlyPrefs.cs
@@ -2,36 +2,14 @@
 
 namespace PrefsUGUI
 {
-    public interface IReadOnlyPrefs<ValType> : IDisposable
+    public interface IReadOnlyPrefs<ValType> : IDisposable, IPrefsCommon
     {
-        #region "PrefsBase"
-        event Action OnValueChanged;
-        event Action OnDisposed;
-
-        Guid PrefsId { get; }
-        string SaveKey { get; }
-        string Key { get; }
-        string GuiLabel { get; }
-        bool Unsave { get; }
-        GuiHierarchy GuiHierarchy { get; }
-        int GuiSortOrder { get; }
-        #endregion
-
         #region "PrefsValueBase"
         ValType Value { get; }
         ValType DefaultValue { get; }
 
         ValType Get();
         ValType GetDefaultValue();
-        #endregion
-
-        #region "PrefsGuiBase"
-        string GuiLabelWithoutAffix { get; }
-        float BottomMargin { get; }
-        float TopMargin { get; }
-        bool VisibleGUI { get; }
-        string GuiLabelPrefix { get; }
-        string GuiLabelSufix { get; }
         #endregion
     }
 }
