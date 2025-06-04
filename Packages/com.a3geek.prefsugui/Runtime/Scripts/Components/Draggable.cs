3bf90fb771ea8b698c812f72e513635ca048b5aa
a3geek <a3geek@gmail.com>
2025-06-04 14:31:37 +0900

    Draggableを移設

--- Packages/com.a3geek.prefsugui/Runtime/Scripts/Components/Draggable.cs
+++ Packages/com.a3geek.prefsugui/Runtime/Scripts/Components/Draggable.cs
@@ -3,24 +3,24 @@ using UnityEngine.EventSystems;
 
 namespace PrefsUGUI.Components
 {
-    [AddComponentMenu("")]
     [DisallowMultipleComponent]
-    public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler
+    public sealed class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler
     {
-        protected Vector2 position = Vector2.zero;
+        private Vector2 position = Vector2.zero;
 
 
-        public virtual void OnBeginDrag(PointerEventData pointerEventData)
+        void IBeginDragHandler.OnBeginDrag(PointerEventData pointerEventData)
         {
             this.position = pointerEventData.position;
         }
 
-        public virtual void OnDrag(PointerEventData pointerEventData)
+        void IDragHandler.OnDrag(PointerEventData pointerEventData)
         {
             var d = pointerEventData.position - this.position;
             var pos = this.transform.position;
 
-            this.transform.position = d + new Vector2(pos.x, pos.y);
+            var v = d + new Vector2(pos.x, pos.y);
+            this.transform.position = new Vector3(v.x, v.y, 0f);
             this.position = pointerEventData.position;
         }
     }
